56bdf8130bd2802218274621492730f5cab7553c
Kite-Styn <tonyci0402@gmail.com>
2022-01-17 11:48:55 +0100

    Javascript optimisation

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,4 @@
+//Récupération de l'ID dans l'url avec URLSearchParams.
 var url = new URL(window.location.href);
 var currentId = url.searchParams.get("id");
 document.getElementById("title").innerHTML=currentId;
@@ -11,16 +12,18 @@ var currentId = search_params.get("id");
 document.getElementById("price").innerHTML=currentId;
 */
 
-fetch("http://localhost:3000/api/products")
+//Appel de l'API pour recevoir les données du produit possédant l'ID récupéré et ajout de ses valeurs dans le html.
+fetch(`http://localhost:3000/api/products/${currentId}`)
 .then(function(res) {
     if (res.ok) {
       return res.json();
     }
 })
 .then(function(value) {
-    var currentIndex = value.findIndex(i => i._id === currentId);
+    document.getElementById("price").innerHTML=value.price;
+    /*var currentIndex = value.findIndex(i => i._id === currentId);
     var currentProduct = value[currentIndex];
-    document.getElementById("price").innerHTML=currentIndex;
+    document.getElementById("price").innerHTML=currentIndex;*/
     /* Alt
     for(var i = 0; i < value.length; i++) {
         if (value[i]._id === currentId) {
--- front/js/script.js
+++ front/js/script.js
@@ -1,13 +1,18 @@
-fetch("http://localhost:3000/api/products")
+//Appel de l'API pour recevoir et insérer les produits dans le html avec une boucle for in.
+let api="http://localhost:3000/api/products"
+fetch(api)
 .then(function(res) {
     if (res.ok) {
       return res.json();
     }
 })
 .then(function(value) {
+    let items = document.getElementById("items");
+    let content = "";
     for (i in value) {
-        document.getElementById("items").innerHTML+='<a href="./product.html?id='+value[i]._id+'"><article><img src='+value[i].imageUrl+' alt='+value[i].altTxt+'/><h3 class="productName">'+value[i].name+'</h3><p class="productDescription">'+value[i].description+'</p></article></a>'
-    }
+        content+='<a href="./product.html?id='+value[i]._id+'"><article><img src='+value[i].imageUrl+' alt='+value[i].altTxt+'/><h3 class="productName">'+value[i].name+'</h3><p class="productDescription">'+value[i].description+'</p></article></a>'
+    };
+    items.innerHTML = content;
 })
 /*
 function rep() {
